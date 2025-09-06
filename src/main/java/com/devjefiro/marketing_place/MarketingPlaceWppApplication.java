8e4cda9f0895db71684e6cd242f6750ce75ed0f9
JEFIRO <150871603+JEFIRO@users.noreply.github.com>
2025-09-06 19:24:42 -0300

    initial commit

---
+++ src/main/java/com/devjefiro/marketing_place/MarketingPlaceWppApplication.java
@@ -0,0 +1,13 @@
+package com.devjefiro.marketing_place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingPlaceWppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingPlaceWppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjefiro/marketing_place/MarketingPlaceWppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjefiro.marketing_place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingPlaceWppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
